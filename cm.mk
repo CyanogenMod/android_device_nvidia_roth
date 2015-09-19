dae40c2ffee4025cc938895102949194ec53995e
Aaron Kling <webgeek1234@gmail.com>
2015-09-18 22:48:51 -0500

    Don't need to override the name and device.

--- cm.mk
+++ cm.mk
@@ -5,8 +5,6 @@ $(call inherit-product, device/nvidia/roth/full_roth.mk)
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=roth \
-    TARGET_DEVICE=roth \
     BUILD_FINGERPRINT="nvidia/thor/roth:5.1/LMY47N/01.00.32208_585.9399:user/release-keys" \
     PRIVATE_BUILD_DESC="thor_gms-user 5.1 LMY47N 01.00.32208_585.9399 release-keys"
 
